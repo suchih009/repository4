31b412512d74d3420a56414791b87fd3a0879499
suchi <suchi@DESKTOP-1C73G36>
2022-01-21 09:52:21 +0530

    First push to the GitHub

---
+++ comcast.githubpractices.sdet28/src/comcast/githubpractices/sdet28/Demo.java
@@ -0,0 +1,13 @@
+package comcast.githubpractices.sdet28;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("launching the browser");
+		System.out.println("Login to application");
+		System.out.println("logout from application");
+
+
+	}
+
+}
